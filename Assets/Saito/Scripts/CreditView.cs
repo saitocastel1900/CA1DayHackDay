7e365312ee8746fc3de6f6440a49f1e49ac3ad6a
saitocastle1900_test <s2023178@cco.kanagawa-it.ac.jp>
2022-10-22 13:38:05 +0900

    タイトル周りの実装（すべて終わった）

---
+++ Assets/Saito/Scripts/CreditView.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditView : MonoBehaviour
+{
+    //button
+    [SerializeField] private Button _openButton;
+    [SerializeField] private Button closeButton;
+    
+    //credit
+    [SerializeField] private GameObject _credit;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _openButton.OnClickAsObservable()
+            .Subscribe(value =>
+            {
+                _credit.gameObject.SetActive(true);
+            }).AddTo(this);
+        closeButton.OnClickAsObservable()
+            .Subscribe(value =>
+            {
+                _credit.gameObject.SetActive(false);
+            }).AddTo(this);
+    }
+}
