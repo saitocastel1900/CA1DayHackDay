a5fa45608ebdf531f113e85464c0dfb171060ffc
saitocastle1900_test <s2023178@cco.kanagawa-it.ac.jp>
2022-10-22 12:05:36 +0900

    Addressableの追加とタイトルシーンへの移動処理を追加しました

---
+++ Assets/Saito/Scripts/AddressableSceneTransitin.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using UnityEngine.AddressableAssets;
+using UnityEngine.SceneManagement;
+
+namespace Saito
+{
+    public class AddressableSceneTransitin : MonoBehaviour
+    {
+        [SerializeField] private AssetReference _scene;
+
+        public async void SceneTransition()
+        {
+            await _scene.LoadSceneAsync(LoadSceneMode.Single).Task;
+            //_scene.ReleaseAsset();
+        }
+
+        void OnDisable()
+        {
+            //_scene?.ReleaseAsset();
+            Debug.Log("OnDisable");
+        }
+
+        private void Start()
+        {
+            this.gameObject.GetComponent<Button>().OnClickAsObservable()
+                .Subscribe(_ => SceneTransition()).AddTo(this);
+        }
+    }
+}
