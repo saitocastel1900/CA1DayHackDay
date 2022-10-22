ee77ce7e9c0d85a10c2d555f22f069de8679bd87
douguya <74404093+douguya@users.noreply.github.com>
2022-10-22 16:59:19 +0900

    no message

--- Assets/imamura/Gimmics.cs
+++ Assets/imamura/Gimmics.cs
@@ -6,7 +6,7 @@ public class Gimmics : MonoBehaviour
 {
     
     public GameObject Gimmick1_Panel;
-
+    public GameObject Gimmick2_Panel;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +29,12 @@ public class Gimmics : MonoBehaviour
 
     }
 
+    public void Gimmick2_Start()//ギミック開始
+    {
+        Gimmick2_Panel.SetActive(true);//パネルを表示
+        Gimmick2_Panel.GetComponent<Smartphone>().setup();//パネル表示で干渉可能になったので、ギミックのセットアップを発動
+
+    }
 
 
 }
